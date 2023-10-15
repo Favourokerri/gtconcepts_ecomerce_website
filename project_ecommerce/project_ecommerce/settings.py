b9bf6e922fdb9f480cdb953fa679e86879e9b4cb
Favour okerri <favourokerri767@gmail.com>
2023-10-15 07:00:52 -0700

    add

--- project_ecommerce/project_ecommerce/settings.py
+++ project_ecommerce/project_ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-%f&o-1q$3ygl_q1r$@kz7ny_h&tm+^w@5oo4m5c1gt_t6j6-@f
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['192.168.43.79', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.43.79', '*', 'localhost', '127.0.0.1']
 
 
 # Application definition
